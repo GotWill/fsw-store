c15aa7116def59534dc60589d939755ce6c14ab7
willian pereira <willianpereira0912@gmail.com>
2023-10-23 22:47:03 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -15,16 +15,16 @@ async function Home() {
     })
 
     return ( 
-        <div className="p-5">
+        <div className="">
             <Image
             src="/banner-home-01.png" 
             width={0} 
             height={0} 
-            className="w-auto h-full"
+            className="w-auto h-full px-5"
             sizes="100vh"
             alt="" />
 
-           <div className="mt-5">
+           <div className="mt-5 px-5">
                 <Categories/>
            </div>
 
