96f790efe3b837221f4be69b8989924257fb7d31
willian pereira <willianpereira0912@gmail.com>
2023-10-23 22:55:43 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,8 +29,17 @@ async function Home() {
            </div>
 
            <div className="mt-8">
+             <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
              <ProductList products={deals}/>
            </div>
+
+           <Image
+            src="/banner-home-02.png" 
+            width={0} 
+            height={0} 
+            className="w-auto h-full px-5"
+            sizes="100vh"
+            alt="Até 55% de desconto em mouses" />
         </div>
      );
 }
