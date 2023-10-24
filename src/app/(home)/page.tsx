247f218103e992e735b077c19cfd7ece3b301fcc
willian pereira <willianpereira0912@gmail.com>
2023-10-23 23:11:05 -0300

    feat: add keyboards section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,46 +2,53 @@ import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
-
+import SectionTitle from "./components/section-title";
+import PromoBanner from "./components/promo-banner";
 
 async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
 
-    const deals = await prismaClient.product.findMany({
-        where: {
-            discountPercentage: {
-                gt: 0
-            }
-        }
-    })
+  const keyBoards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
 
-    return ( 
-        <div className="">
-            <Image
-            src="/banner-home-01.png" 
-            width={0} 
-            height={0} 
-            className="w-auto h-full px-5"
-            sizes="100vh"
-            alt="" />
+  return (
+    <div className="">
+      <PromoBanner 
+        src="/banner-home-01.png" 
+        alt="" 
+        />
 
-           <div className="mt-5 px-5">
-                <Categories/>
-           </div>
+      <div className="mt-5 px-5">
+        <Categories />
+      </div>
 
-           <div className="mt-8">
-             <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
-             <ProductList products={deals}/>
-           </div>
+      <div className="mt-8">
+        <SectionTitle>Ofertas</SectionTitle>
+        <ProductList products={deals} />
+      </div>
 
-           <Image
-            src="/banner-home-02.png" 
-            width={0} 
-            height={0} 
-            className="w-auto h-full px-5"
-            sizes="100vh"
-            alt="Até 55% de desconto em mouses" />
-        </div>
-     );
+      <PromoBanner
+        src="/banner-home-02.png"
+        alt="Até 55% de desconto em mouses"
+      />
+
+      <div className="mt-8">
+        <SectionTitle>tECLADOS</SectionTitle>
+        <ProductList products={keyBoards} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
