cbcc40f19648aa9771b9a405894a150000340930
willian pereira <willianpereira0912@gmail.com>
2023-10-26 00:38:53 -0300

    feat: add CartItem components

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,9 +1,10 @@
-'use client'
+"use client";
 
+import { ProductWithTotalPrice } from "@/helpers/porduct";
 import { Product } from "@prisma/client";
 import { ReactNode, createContext, useState } from "react";
 
-interface CardProduct extends Product {
+export interface CardProduct extends ProductWithTotalPrice {
   quantity: number;
 }
 
@@ -12,7 +13,7 @@ interface ICartContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
-  addProductsToCart: (product: CardProduct) => void
+  addProductsToCart: (product: CardProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,16 +21,36 @@ export const CartContext = createContext<ICartContext>({
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
-  addProductsToCart: () => {}
+  addProductsToCart: () => {},
 });
 
-const CartProvider = ({ children }: {children: ReactNode}) => {
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CardProduct[]>([]);
 
-  const [products, setProducts] = useState<CardProduct[]>([])
+  const addProductsToCart = (product: CardProduct) => {
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
 
-  const addProductsToCart = (product: CardProduct) =>{
-    setProducts((prev) => [...prev, product])
-  }
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+
+      return;
+    }
+
+    setProducts((prev) => [...prev, product]);
+  };
 
   return (
     <CartContext.Provider
@@ -38,10 +59,10 @@ const CartProvider = ({ children }: {children: ReactNode}) => {
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
-        addProductsToCart
+        addProductsToCart,
       }}
     >
-        {children}
+      {children}
     </CartContext.Provider>
   );
 };
