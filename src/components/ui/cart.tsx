e98e600f91aa556e0618116beb4d56e7aacfee28
willian pereira <willianpereira0912@gmail.com>
2023-12-09 16:56:45 -0300

    feat: ensure cart products  can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,11 +5,13 @@ import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/porduct";
 import { Separator } from "@radix-ui/react-separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
   const { products, subTotal, total, totalDiscount } = useContext(CartContext);
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -18,19 +20,21 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              product={computeProductTotalPrice(product) as any}
-              key={product.id}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Você ainda não tem nenhum produto no carrinho.
-          </p>
-        )}
+      <div className="flex h-full max-h-full overflow-hidden flex-col gap-5">
+        <ScrollArea className="h-full">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <CartItem
+                product={computeProductTotalPrice(product) as any}
+                key={product.id}
+              />
+            ))
+          ) : (
+            <p className="text-center font-semibold">
+              Você ainda não tem nenhum produto no carrinho.
+            </p>
+          )}
+        </ScrollArea>
       </div>
 
       {products.length > 0 && (
@@ -59,6 +63,8 @@ const Cart = () => {
             <p>Total</p>
             <p>R$ {total.toFixed(2)}</p>
           </div>
+
+          <Button className="uppercase font-bold mt-7">Finalizar comprar</Button>
         </div>
       )}
     </div>
